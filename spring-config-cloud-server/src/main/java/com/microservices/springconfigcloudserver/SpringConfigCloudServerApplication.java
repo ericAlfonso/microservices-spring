d166a6f94f0e58415b60fbd9b2676b231b0e8a65
Eric <erictutorias@gmail.com>
2019-11-18 15:32:58 -0600

    Second commit connecting config server to client and env configurations

--- spring-config-cloud-server/src/main/java/com/microservices/springconfigcloudserver/SpringConfigCloudServerApplication.java
+++ spring-config-cloud-server/src/main/java/com/microservices/springconfigcloudserver/SpringConfigCloudServerApplication.java
@@ -2,8 +2,10 @@ package com.microservices.springconfigcloudserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringConfigCloudServerApplication {
 
 	public static void main(String[] args) {
