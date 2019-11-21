7e4772baf127fe41dbab1408cba357e014405deb
Eric <erictutorias@gmail.com>
2019-11-21 16:43:14 -0600

    API Gateway (zuul) implemented

--- port-module-service/src/main/java/com/microservices/portmoduleservice/client/ScheduleModuleServiceClient.java
+++ port-module-service/src/main/java/com/microservices/portmoduleservice/client/ScheduleModuleServiceClient.java
@@ -7,11 +7,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.microservices.portmoduleservice.beans.PortDetails;
 
-@FeignClient(name = "schedule-module-service")
+//@FeignClient(name = "schedule-module-service")
+@FeignClient(name = "zuul-api-gateway-server")
 @RibbonClient(name = "schedule-module-service")
 public interface ScheduleModuleServiceClient {
 
-	@GetMapping("/schedule/port/{name}")
+	@GetMapping("/schedule-module-service/schedule/port/{name}")
 	public PortDetails getPortScheduled(
 				@PathVariable("name") final String name);
 }
